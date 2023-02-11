ed830d9e41fa41d018ce5d2e1b0652509d2a2644
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-02-11 12:25:52 -0300

    subindo conteudo da aula

---
+++ src/sintaxes_variaveis_e_fluxos/TestaConversao.java
@@ -0,0 +1,11 @@
+package sintaxes_variaveis_e_fluxos;
+
+public class TestaConversao {
+		
+		public static void main(String[] args) {
+			double salario = 1270.50;
+			int valor = (int) salario;
+			
+		}
+		
+}
