8a706572f4e9d51b36a264cf6331caaaf66eb146
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-02-11 16:13:43 -0300

    aprendendo sobre valores

--- src/sintaxes_variaveis_e_fluxos/TestaValores.java
+++ src/sintaxes_variaveis_e_fluxos/TestaValores.java
@@ -7,8 +7,19 @@ public class TestaValores {
 		segundo = primeiro;
 		primeiro = 10;
 
-		// quanto vale o segundo?
+	      char letra = 'a';
+	        System.out.println(letra);
 
-		System.out.println(segundo);
+	        char valor = 65;                    // Compila!
+	        System.out.println(valor);
+
+	        valor = (char) (valor + 1);         // Compila!
+	        System.out.println(valor);
+
+	        String palavra = "alura cursos online de tecnologia";
+	        System.out.println(palavra);
+
+	        palavra = palavra + 2020;
+	        System.out.println(palavra);
 	}
 }
