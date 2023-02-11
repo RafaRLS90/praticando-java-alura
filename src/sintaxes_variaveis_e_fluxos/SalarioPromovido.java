49048394bd06b3f86b91d08dca9c84f8f8022feb
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-02-11 18:00:56 -0300

    exercícios de fixação

---
+++ src/sintaxes_variaveis_e_fluxos/SalarioPromovido.java
@@ -0,0 +1,22 @@
+package sintaxes_variaveis_e_fluxos;
+
+public class SalarioPromovido {
+	public static void main(String[] args) {
+
+        boolean foiPromovido = true;
+
+       
+        
+        if(foiPromovido) {
+            double salario = 4200.0;
+        
+            System.out.println(salario);
+        } else {
+            double salario = 3800.0;
+            
+            System.out.println(salario);
+        }
+
+        
+    }
+}
