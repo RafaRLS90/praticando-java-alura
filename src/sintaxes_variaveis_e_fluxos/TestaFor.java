ad030bba248b6dafbb25552d8fb2a149ae7b9f88
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-02-11 22:47:03 -0300

    Laços

---
+++ src/sintaxes_variaveis_e_fluxos/TestaFor.java
@@ -0,0 +1,10 @@
+package sintaxes_variaveis_e_fluxos;
+
+public class TestaFor {
+
+    public static void main(String[] args) {
+        for(int contador = 0; contador <= 10; contador++) {
+            System.out.println(contador);
+        }
+    }
+}
